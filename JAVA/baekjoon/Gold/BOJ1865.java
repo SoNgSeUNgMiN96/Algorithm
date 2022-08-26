a2c86b35b6bfc50afa1d6cc2fab8c34b95a86b65
SoNgSeUNgMiN96 <tmd9544@gmail.com>
2022-08-26 19:06:54 +0900

    알고리즘 풀이 추가

--- JAVA/baekjoon/Gold/BOJ1865.java
+++ JAVA/baekjoon/Gold/BOJ1865.java
@@ -3,6 +3,8 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ1865 {
@@ -16,59 +18,63 @@ public class BOJ1865 {
         }
 
     }
-     public static void main(String[] args) throws Exception{
-         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-         StringTokenizer st;
-         int TC = Integer.parseInt(br.readLine());
-         ArrayList<Vector> vectors;
-         int [] dist;
 
-         int N, M, W, a, b, c;
-         loop : while (TC-->0) {
-             st = new StringTokenizer(br.readLine());
-             N = Integer.parseInt(st.nextToken());
-             M = Integer.parseInt(st.nextToken());
-             W = Integer.parseInt(st.nextToken());
-             dist = new int[N+1];
-             for (int i = 2; i < dist.length; i++) {
-                 dist[i] = Integer.MAX_VALUE/2 -1;
-             }
-             vectors = new ArrayList<>();
+    public static void main(String[] args) throws Exception {
 
-             for (int i = 0; i < M; i++) {
-                 st = new StringTokenizer(br.readLine());
-                 a = Integer.parseInt(st.nextToken());
-                 b = Integer.parseInt(st.nextToken());
-                 c = Integer.parseInt(st.nextToken());
-                 vectors.add(new Vector(a,b,c));
-                 vectors.add(new Vector(b,a,c));
-             }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int TC = Integer.parseInt(br.readLine());
+        ArrayList<Vector> vectors;
+        int[] dist;
 
-             for (int i = 0; i < W; i++) {
-                 st = new StringTokenizer(br.readLine());
-                 a = Integer.parseInt(st.nextToken());
-                 b = Integer.parseInt(st.nextToken());
-                 c = Integer.parseInt(st.nextToken());
-                 vectors.add(new Vector(a,b,-c));
-             }
+        int N, M, W, a, b, c;
+        loop:
+        while (TC-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            W = Integer.parseInt(st.nextToken());
+            dist = new int[N + 1];
+            for (int i = 2; i < dist.length; i++) {
+                dist[i] = Integer.MAX_VALUE / 2 - 1;
+            }
+            vectors = new ArrayList<>();
 
-             for (int i = 0; i < M+W-1; i++) {
-                 for (Vector vector : vectors) {
-                     if(dist[vector.from]+ vector.w< dist[vector.to]){
-                         dist[vector.to] = dist[vector.from]+ vector.w;
-                     }
-                 }
-             }
+            for (int i = 0; i < M; i++) {
+                st = new StringTokenizer(br.readLine());
+                a = Integer.parseInt(st.nextToken());
+                b = Integer.parseInt(st.nextToken());
+                c = Integer.parseInt(st.nextToken());
+                vectors.add(new Vector(a, b, c));
+                vectors.add(new Vector(b, a, c));
+            }
 
-             for (Vector vector : vectors) {
-                 if(dist[vector.from]+ vector.w< dist[vector.to]){
-                     bw.write("YES\n");
-                     continue loop;
-                 }
-             }
-             bw.write("NO\n");
-         }
-         bw.flush();
-     }
+            for (int i = 0; i < W; i++) {
+                st = new StringTokenizer(br.readLine());
+                a = Integer.parseInt(st.nextToken());
+                b = Integer.parseInt(st.nextToken());
+                c = Integer.parseInt(st.nextToken());
+                vectors.add(new Vector(a, b, -c));
+            }
+
+            for (int i = 0; i < M + W - 1; i++) {
+                for (Vector vector : vectors) {
+                    if (dist[vector.from] + vector.w < dist[vector.to]) {
+                        dist[vector.to] = dist[vector.from] + vector.w;
+                    }
+                }
+            }
+
+            for (Vector vector : vectors) {
+                if (dist[vector.from] + vector.w < dist[vector.to]) {
+                    bw.write("YES\n");
+                    continue loop;
+                }
+            }
+            bw.write("NO\n");
+        }
+        bw.flush();
+    }
 }
+
